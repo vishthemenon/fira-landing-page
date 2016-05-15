1cc53961092611f4c32cb9bb5f6351bdc8b244a1
Vishnu R Menon <vishthemenon@gmail.com>
2016-05-15 12:12:03 +0800

    bird animation

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
-var colors = ["#34314c", "#34314c", "#ff7473", "#ffc952", "#47b8e0"]
+var colors = ["#34314c", "#ff7473", "#ffc952", "#47b8e0", "#47b8e0"]
 var i = 0
 $(function(){
         $("#typed").typed({
             strings: ["fira talks.", "fira handles.", "fira manages.", "fira learns.", "fira changes.", "fira changes"],
             typeSpeed: 25,
             callback: function() {
-              $("#subtitle").fadeIn("slow");
+              $(".hero-show").fadeIn("slow");
+              $("#logo").fadeIn("slow");
               $("#hero > .fa-arrow-down").fadeIn("slow")
             },
             onStringTyped: function() {
@@ -14,3 +15,14 @@ $(function(){
             }
         });
     });
+
+$(window).scroll(function(){
+  var wScroll = $(this).scrollTop();
+  var zHeight = $('body').height();
+  if(wScroll > 1700){
+    $(".bird").css("transform", "none")
+    $("#smallbird").css("transform", "scale(0.6)")
+    $("#smallbird").css("opacity", "0.7")
+
+  }
+})
